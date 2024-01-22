16fef2085fa1d58ced46af8710157b329a11e189
elduke717 <spacematcha@proton.me>
2024-01-22 15:19:27 -0500

    add some tailwind styles and color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-dark-purple': '#b300b3',
+				'secondary-dark-purple': '#800080',
+				'light-grey': '#e7e7e7',
+				'semi-transparent-light-grey': '#e7e7e799',
+				'vibrant-yellow': '#9f9f00',
+				'deep-blue-black': '#15202b',
+				'semi-transparent-secondary-dark-purple': '#80008099',
+			  }
+		},
 	},
 	plugins: [],
 }
